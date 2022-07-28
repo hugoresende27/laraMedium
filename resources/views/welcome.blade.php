20f0f242931f171ebe634d1b08f20b21af72d732
hugoresende27 <103998284+hugoresendehcpro@users.noreply.github.com>
2022-07-28 21:06:43 +0100

    alteraco 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laraveasdadasddasdasdaaddasdasdasdasdadl</title>
         
         <title>Laravel</title>
         <title>Laravel</title>
